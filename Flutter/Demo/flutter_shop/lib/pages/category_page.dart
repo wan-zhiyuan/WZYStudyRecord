3c476ab8b0804596643eac255d5e5a97b67ee26a
wan-zhiyuan <w13585643941@163.com>
2019-04-25 09:28:41 +0800

    update

--- Flutter/Demo/flutter_shop/lib/pages/category_page.dart
+++ Flutter/Demo/flutter_shop/lib/pages/category_page.dart
@@ -1,13 +1,98 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
 import 'dart:convert';//引入后可使用json
+import '../model/category.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class CategoryPage extends StatelessWidget {
+class CategoryPage extends StatefulWidget {
+  @override
+  _CategoryPageState createState() => _CategoryPageState();
+}
 
+class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('分类页面'),),
+      appBar: AppBar(title: Text('商品分类'),),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            LeftCategoryNav(),
+            Container(),
+          ],
+        ),
+      ),
     );
   }
+
+
+}
+
+//左侧大类导航
+class LeftCategoryNav extends StatefulWidget {
+
+
+  _LeftCategoryNavState createState() => _LeftCategoryNavState();
+}
+
+class _LeftCategoryNavState extends State<LeftCategoryNav> {
+
+  List list = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //左侧导航栏初始化时请求数据
+    _getCategory();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil().setWidth(180),//左侧导航栏的宽度
+      decoration: BoxDecoration(
+        border: Border(
+          right: BorderSide(width: 1,color: Colors.black12)
+        )
+      ),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context,index){
+          return _leftInkWell(index);
+        },
+      ),
+    );
+  }
+
+  Widget _leftInkWell(int index){
+
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left: 10,top: 20),//内边距，文字距离边框的距离
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(width: 1,color:Colors.black12)
+          )
+        ),
+        child: Text(list[index].mallCategoryName,style: TextStyle(fontSize: ScreenUtil().setSp(28)),),//style等下写
+      ),
+      );
+  }
+
+  //内部方法，测试接口
+  void _getCategory() async{
+    await request('getCategory').then((val){
+      var data = json.decode(val.toString());
+      CategoryModel category = CategoryModel.fromJson(data);
+      setState(() {
+       list = category.data; 
+      });
+      // list.data.forEach((item)=>print(item.mallCategoryName));//打印所有大类的名称
+    });
+  }
+
 }
