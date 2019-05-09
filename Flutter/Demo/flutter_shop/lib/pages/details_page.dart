549cef893461e8bbaec4e9821b6646b114bcd367
wan-zhiyuan <w13585643941@163.com>
2019-05-09 11:34:49 +0800

    update 49

--- Flutter/Demo/flutter_shop/lib/pages/details_page.dart
+++ Flutter/Demo/flutter_shop/lib/pages/details_page.dart
@@ -5,6 +5,7 @@ import 'detail_page/details_top_area.dart';
 import 'detail_page/details_explain.dart';
 import 'detail_page/details_tabbar.dart';
 import 'detail_page/details_web.dart';
+import 'detail_page/details_bottom.dart';
 
 class DetailsPage extends StatelessWidget {
   final String goodsId;
@@ -26,16 +27,26 @@ class DetailsPage extends StatelessWidget {
         future: _getBackInfo(context),
         builder: (context,snapshot){
           if(snapshot.hasData){
-            return Container(
-              child: ListView(
-                children: <Widget>[
-                  DetailsTopArea(),
-                  DetailsExplain(),
-                  DetailsTabbar(),
-                  DetailsWeb(),
-                ],
-              ),
+            return Stack(
+              children: <Widget>[
+                Container(
+                  child: ListView(
+                    children: <Widget>[
+                      DetailsTopArea(),
+                      DetailsExplain(),
+                      DetailsTabbar(),
+                      DetailsWeb(),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  child: DetailsBottom(),
+                ),
+              ],
             );
+            
           }else{
             return Text('加载中......');
           }
