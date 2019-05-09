99003018a1e495c2d1390ad137e75fb4be20f1a6
wan-zhiyuan <w13585643941@163.com>
2019-05-09 10:14:45 +0800

    update 48

--- Flutter/Demo/flutter_shop/lib/pages/detail_page/details_web.dart
+++ Flutter/Demo/flutter_shop/lib/pages/detail_page/details_web.dart
@@ -8,10 +8,29 @@ class DetailsWeb extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var goodsDetails = Provide.value<DetailsInfoProvide>(context).goodsInfo.data.goodInfo.goodsDetail;
-    return Container(
-      child: Html(
-        data: goodsDetails,
-      ),
+    return Provide<DetailsInfoProvide>(
+      builder: (context,child,val){
+        var isLeft = Provide.value<DetailsInfoProvide>(context).isLeft;
+        //根据isLeft控制web页面显示内容
+        if(isLeft){
+          return Container(
+            child: Html(
+              data: goodsDetails,
+            ),
+          );
+        }else{
+          return Container(
+            width: ScreenUtil().setWidth(750),
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.center,//child居中
+            child: Text(
+              '暂时没有数据。'
+            ),
+          );
+        }
+      },
     );
+    
+    
   }
 }
