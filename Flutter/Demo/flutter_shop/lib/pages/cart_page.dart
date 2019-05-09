fc7b8067c5aa3fd4cc5d0a4dddacbad2eb244af9
wan-zhiyuan <w13585643941@163.com>
2019-05-09 15:13:39 +0800

    update 50 shared_preferences测试

--- Flutter/Demo/flutter_shop/lib/pages/cart_page.dart
+++ Flutter/Demo/flutter_shop/lib/pages/cart_page.dart
@@ -1,52 +1,74 @@
 import 'package:flutter/material.dart';
-import 'package:provide/provide.dart';
-import '../provide/counter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class CartPage extends StatelessWidget {
+class CartPage extends StatefulWidget {
+  @override
+  _CartPageState createState() => _CartPageState();
+}
 
+class _CartPageState extends State<CartPage> {
+  
+  List<String> testList = [];
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // body: Center(child: Text('购物车页面'),),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Number(),
-            MyButton(),
-          ],
-        ),
+    _show();
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container( //使用ListView的时候需要给它设定一个高度，所以外面包一个Container
+            height: 500.0,
+            child: ListView.builder(
+              itemCount: testList.length,
+              itemBuilder: (context,index){
+                return ListTile(
+                  title: Text(testList[index]),
+                );
+              },
+            ),
+          ),
+          RaisedButton(
+            onPressed: (){_add();},
+            child: Text('增加'),
+          ),
+          RaisedButton(
+            onPressed: (){_clear();},
+            child: Text('清空'),
+          )
+        ],
       ),
     );
   }
-}
 
-class Number extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 200),
-      child: Provide<Counter>(
-        builder: (context,child,counter){
-          return Text(
-            '${counter.value}',
-            style: Theme.of(context).textTheme.display1,
-          );
-        },
-        ),
-    );
+  //增删改查都需要使用异步方法，因为SharedPreferences的初始化需要等待
+  //增加方法
+  void _add() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();//初始化
+    String temp = "你好，ibarrel";
+    testList.add(temp);
+    prefs.setStringList('testInfo', testList);
+    _show();
   }
-}
 
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: RaisedButton(
-        onPressed: (){
-          Provide.value<Counter>(context).increment();
-        },
-        child: Text('递增'),
-      ),
-    );
+  //查询方法
+  void _show() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.getStringList('testInfo')!=null) {
+      setState(() {
+       testList = prefs.getStringList('testInfo'); 
+      });
+    } else {
+
+    }
+  }
+
+  //删除方法
+  void _clear() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //prefs.clear();//删除全部
+    prefs.remove('testInfo');//删除某一项
+    setState(() {
+     testList=[]; 
+    });
   }
 }
