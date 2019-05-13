99374ad9b2edf4d9216de88dd35b6dc8936c19b8
wan-zhiyuan <w13585643941@163.com>
2019-05-13 15:40:47 +0800

    update 55

--- Flutter/Demo/flutter_shop/lib/pages/cart_page.dart
+++ Flutter/Demo/flutter_shop/lib/pages/cart_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provide/provide.dart';
 import '../provide/cart.dart';
 import './cart_page/cart_item.dart';
+import './cart_page/cart_bottom.dart';
 
 class CartPage extends StatelessWidget {
   @override
@@ -15,12 +16,23 @@ class CartPage extends StatelessWidget {
         builder: (context,snapshot){
           if (snapshot.hasData) {
             List cartList=Provide.value<CartProvide>(context).cartList;
-            return ListView.builder(
-              itemCount: cartList.length,
-              itemBuilder: (context,index){
-                return CartItem(cartList[index]);
-              },
+
+            return Stack(
+              children: <Widget>[
+                ListView.builder(
+                  itemCount: cartList.length,
+                  itemBuilder: (context,index){
+                    return CartItem(cartList[index]);
+                  },
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  child: CartBottom(),
+                ),
+              ],
             );
+
 
           } else {
             return Text('正在加载',style: TextStyle(color: Colors.black),);
