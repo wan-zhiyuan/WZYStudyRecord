3d56782decb87fd416415ca1aeb13b1223ded5f6
wan-zhiyuan <w13585643941@163.com>
2019-05-17 09:42:41 +0800

    update 64、会员中心头部

--- Flutter/Demo/flutter_shop/lib/pages/member_page.dart
+++ Flutter/Demo/flutter_shop/lib/pages/member_page.dart
@@ -1,22 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provide/provide.dart';
-import '../provide/counter.dart';
 
 class MemberPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Provide<Counter>(
-          builder: (context,child,counter){
-            return Text(
-              '${counter.value}',
-              style: Theme.of(context).textTheme.display1,
-            );
-          },
-        ),
-        ),
+      appBar: AppBar(
+        title: Text('会员中心'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _topHeader(),
+        ],
+      ),
+    );
+  }
+
+  Widget _topHeader(){
+
+    return Container(
+      width: ScreenUtil().setWidth(750),
+      padding: EdgeInsets.all(20),
+      color: Colors.pinkAccent,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            child: ClipOval(
+              child: Image.network('http://blogimages.jspang.com/blogtouxiang1.jpg'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              '技术胖',
+              style: TextStyle(fontSize: ScreenUtil().setSp(36),color: Colors.black54,),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
