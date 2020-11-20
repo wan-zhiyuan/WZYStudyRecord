075834c123dbe2f8bfadc0c5557877383350b2c8
wan-zhiyuan <w13585643941@163.com>
2020-11-21 01:11:50 +0800

    udp

--- React/react_blog/blog/pages/detailed.js
+++ React/react_blog/blog/pages/detailed.js
@@ -70,7 +70,7 @@ Chicken Chicken Chicken Chicken Chicken Chicken.
 
 let markdown =
   '' +
-  '# p01:课程介绍和环境搭建\n' +
+  '## p01:课程介绍和环境搭建\n' +
   '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
   '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
   '**这是加粗的文字**\n\n' +
@@ -78,28 +78,28 @@ let markdown =
   '***这是斜体加粗的文字***\n\n' +
   '~~这是加删除线的文字~~ \n\n' +
   '\`console.log(111)\` \n\n' +
-  '# p02:来个Hello World 初始Vue3.0\n' +
+  '## p02:来个Hello World 初始Vue3.0\n' +
   '> aaaaaaaaa\n' +
   '>> bbbbbbbbb\n' +
   '>>> cccccccccc\n' +
   '***\n\n\n' +
-  '# p03:Vue3.0基础知识讲解\n' +
+  '## p03:Vue3.0基础知识讲解\n' +
   '> aaaaaaaaa\n' +
   '>> bbbbbbbbb\n' +
   '>>> cccccccccc\n\n' +
-  '# p04:Vue3.0基础知识讲解\n' +
+  '## p04:Vue3.0基础知识讲解\n' +
   '> aaaaaaaaa\n' +
   '>> bbbbbbbbb\n' +
   '>>> cccccccccc\n\n' +
-  '#5 p05:Vue3.0基础知识讲解\n' +
+  '## p05:Vue3.0基础知识讲解\n' +
   '> aaaaaaaaa\n' +
   '>> bbbbbbbbb\n' +
   '>>> cccccccccc\n\n' +
-  '# p06:Vue3.0基础知识讲解\n' +
+  '## p06:Vue3.0基础知识讲解\n' +
   '> aaaaaaaaa\n' +
   '>> bbbbbbbbb\n' +
   '>>> cccccccccc\n\n' +
-  '# p07:Vue3.0基础知识讲解\n' +
+  '## p07:Vue3.0基础知识讲解\n' +
   '> aaaaaaaaa\n' +
   '>> bbbbbbbbb\n' +
   '>>> cccccccccc\n\n' +
@@ -132,7 +132,7 @@ const Detailed = () => (
             </div>
             <div className='detailed_content'>
               <ReactMarkdown
-                children={article}
+                children={markdown}
                 allowDangerousHtml={false} />
             </div>
           </div>
@@ -147,7 +147,7 @@ const Detailed = () => (
             <div className='nav_title'>文章目录</div>
             <MarkNav
               className='article_menu'
-              source={article}
+              source={markdown}
               ordered={false} // 是否带编号
             />
           </div>
