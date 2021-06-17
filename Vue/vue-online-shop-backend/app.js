59f2cd82f9ac5cf0e209ff1c981e20249ed4e60d
wan-zhiyuan <13585643941@163.com>
2021-06-17 09:52:34 +0800

    update

--- Vue/vue-online-shop-backend/app.js
+++ Vue/vue-online-shop-backend/app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const api = require('./routes/api');
 
 // 初始化express实例
 var app = express();
@@ -14,6 +16,26 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// 连接数据库
+// mongoose.set('useCreateIndex', true);
+mongoose.connect(`mongodb://localhost:27017/test`, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("数据库连接成功"))
+  .catch(() => console.log("数据库连接失败"));
+
+// CORS config here
+app.all('/*', function (req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,14 +46,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 // 导入子路由中间件
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/v1', api);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
